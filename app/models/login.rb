95742f1ebeaefbc7deb228704cf0a9bfe149f064
Sebastian Quintero <squintero69155@umanizales.edu.co>
2019-03-05 14:35:22 -0500

    Se le da longitud al username

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,5 +1,5 @@
 class Login < ApplicationRecord
-    validates :username, presence: true, uniqueness:{case_sensitive: false}
+    validates :username, presence: true, uniqueness:{case_sensitive: false}, length: { minimum: 3, maximum: 50}
     VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, confirmation: true, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
     
