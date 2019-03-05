f27486d4180f2e9f6301b1ba74d1b97f207346e8
juanf <jufe357@hotmail.com>
2019-03-05 12:14:43 -0500

    se realiza las validaciones en las casillas de login

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,2 +1,6 @@
 class Login < ApplicationRecord
+    validates :username, presence: true, uniqueness:{case_sensitive: false}
+    VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, confirmation: true, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+    
 end
