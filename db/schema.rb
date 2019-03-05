cceef005c4d817d82afc426e3cac461c1da24776
juanf <jufe357@hotmail.com>
2019-03-05 09:58:17 -0500

    se migra a la base de datos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_210817) do
+ActiveRecord::Schema.define(version: 2019_03_05_145504) do
+
+  create_table "logins", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
