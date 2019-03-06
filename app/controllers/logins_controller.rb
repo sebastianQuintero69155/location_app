d450e566297efdb84009a034082ca1c447435f29
juanf <jufe357@hotmail.com>
2019-03-06 11:02:56 -0500

    cambios se le quita el campo password para poder generar el login con btcrypt

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -69,6 +69,6 @@ class LoginsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def login_params
-      params.require(:login).permit(:email, :password, :username)
+      params.require(:login).permit(:email, :username)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2019_03_05_145504) do
 
   create_table "logins", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
