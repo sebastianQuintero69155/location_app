d6d363516d282772dbe304d4d6add232c03a5554
juanf <jufe357@hotmail.com>
2019-03-07 10:40:03 -0500

    se crea mensajes de erros de inicio de sesion e inicia sesion enviadolo a la ruta login_path queda pendiente crear la vista donde se visualizara el mapa

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,23 @@ class SessionsController < ApplicationController
     end
 
     def create 
+        login = Login.find_by(email: params[:session][:email].downcase)
+        if login && login.authenticate(params[:session][:password])
+            session[:login_id] = login.id
+            flash[:success] = "you have successfully loggen in"
+            redirect_to login_path(login)
 
-        render 'new'
-        
+        else
+            flash.now[:danger] = "There was something wrong whit your login information"
+           render 'new' 
 
+        end
     end
 
     def destroy
+        session[:login_id] = nil
+        flash[:success] = "you have logged out"
+        redirect_to root_path 
 
     end
 end
