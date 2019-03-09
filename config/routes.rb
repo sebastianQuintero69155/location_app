0561a421abebe8d1a1151da06b52758cca0a4cec
Sebastian Quintero <squintero69155@umanizales.edu.co>
2019-03-09 11:33:53 -0500

    MVC login is removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   resources :logins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
-  get 'singup', to: "logins#new"
-  get 'acces', to: 'sessions#new'
-  post 'acces', to: 'sessions#create'
-  delete 'acces', to: 'sessions#destroy'
-  get 'maps', to: 'pages#maps'
 end
