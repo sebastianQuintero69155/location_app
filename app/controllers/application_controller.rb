8b37e6a29d8ec540e4097face779eb2ed5f6ede9
juanf <jufe357@hotmail.com>
2019-03-11 10:56:31 -0500

    the route is done in the pagein the contoroller aplication once logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    protected
+    def after_sign_in_path_for(resource)
+      request.env['omniauth.origin'] || stored_location_for(resource) || places_path
+    end
 end
