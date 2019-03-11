39eb8a604a64198474dd3c450fc035360aa1f96e
juanf <jufe357@hotmail.com>
2019-03-11 10:35:13 -0500

    test

--- spec/views/places/index.html.erb_spec.rb
+++ spec/views/places/index.html.erb_spec.rb
@@ -19,7 +19,5 @@ RSpec.describe "places/index", type: :view do
   it "renders a list of places" do
     render
     assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
   end
 end
