e0f687878c93518401d83edd99c267bccf4e5620
Sebastian Quintero <squintero69155@umanizales.edu.co>
2019-03-11 12:30:59 -0500

    add view in the home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
     protected
     def after_sign_in_path_for(resource)
       request.env['omniauth.origin'] || stored_location_for(resource) || places_path
