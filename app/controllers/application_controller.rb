4bb7dec58330e769f734858cb26210d58b304ed1
Sebastian Quintero <squintero69155@umanizales.edu.co>
2019-03-12 14:34:23 -0500

    exceptions are organized in the views and designs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:home]
     protected
     def after_sign_in_path_for(resource)
       request.env['omniauth.origin'] || stored_location_for(resource) || places_path
